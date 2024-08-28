8008915003650ba4c9eecb5e458cba990a78f6f4
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-08-28 20:21:31 -0300

    commit

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
-import NavegacaoDrawer from './src/navegacao/drawer';
+import NavegacaoDrawer from "./src/navegacao/drawer";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavegacaoDrawer />
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <View style={styles.container}>
+         <NavegacaoDrawer />
+         <StatusBar style="auto" />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#aaa', 
-    // alignItems: 'center',
-    // justifyContent: 'center', 
-    padding: 18, 
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#aaa",
+      // alignItems: 'center',
+      // justifyContent: 'center',
+      padding: 18,
+   },
 });
-
-/*
-{
-  nomes.map((pessoa, pos) => <Mensagem nome={pessoa.nome} pos={pos} idade={pessoa.idade} />)
-}     
-*/
